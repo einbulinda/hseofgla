d8199d4c53f926c7c8accf154a87d9a1a4c17c32
einbulinda <einbulinda@gmail.com>
2020-12-21 11:29:59 +0300

    Added Redux to Product Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,14 @@
-import data from "./data";
 import { compose, createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import { productListReducer } from "./reducers/productReducers";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducers";
 
 const initialState = {};
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
